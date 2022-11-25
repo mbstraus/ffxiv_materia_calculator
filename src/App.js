a5dfee06020e6e2ff8914da2abb079f355034d33
brainchewer <mathew.strauss@gmail.com>
2022-11-25 13:22:12 -0700

    Moved data to AWS for lols, more refinements.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-// To start client: npm start
-// To start server: mvnw spring-boot:run
-
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import './App.css';
-import { itemData } from './data/ItemData.js';
-import { materia } from './data/Materia.js';
-import { jobs } from './data/Jobs.js';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import * as AWS from 'aws-sdk';
+
+const configuration = {
+    region: "us-west-2",
+    secretAccessKey: "fgcWpnewBlLNRp+e18ZgoPfjWQ4rZdbN8PNoGjoY",
+    accessKeyId: "AKIAUNIJCUO76TGRMIZ3"
+};
+
+AWS.config.update(configuration);
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 const defaultGear = {
     Primary: { name: "" },
@@ -22,13 +24,67 @@ const defaultGear = {
     Necklace: { name: "" },
     Bracelets: { name: "" },
     LeftRing: { name: "" },
-    RightRing: { name: "" }
+    RightRing: { name: "" },
+    Food: { name: "" }
 };
+
+const defaultAutomationConfiguration = {
+    control: { target: "3280", minimize: false, priority: 0 },
+    craftsmanship: { target: "3700", minimize: true, priority: 1 },
+    cp: { target: "564", minimize: true, priority: 2 },
+    firstOvermeldRank: 10,
+    overmeldRank: 9
+}
+
+function GetSortOrder(prop) {
+    return function(a, b) {
+        if (a[prop] > b[prop]) {
+            return 1;
+        } else if (a[prop] < b[prop]) {
+            return -1;
+        }
+        return 0;
+    }
+}
 
 function App() {
     const [ selectedJob, setSelectedJob ] = useState({});
     const [ hasSoulCrystal, setHasSoulCrystal ] = useState(false);
     const [ selectedGear, setSelectedGear ] = useState(defaultGear);
+    const [ automationConfig, setAutomationConfig ] = useState(defaultAutomationConfiguration);
+    const [ itemData, setItemData ] = useState([]);
+    const [ materia, setMateria ] = useState([]);
+    const [ jobs, setJobs ] = useState([]);
+    useEffect(() => {
+        docClient.scan({ TableName: "FFXIV_Items"}, function(err, data) {
+            if (!err) {
+                setItemData(data.Items.sort(GetSortOrder("name")));
+            } else {
+                console.log(err);
+                return [];
+            }
+        });
+    }, []);
+    useEffect(() => {
+        docClient.scan({ TableName: "FFXIV_Materia"}, function(err, data) {
+            if (!err) {
+                setMateria(data.Items.sort(GetSortOrder("name")));
+            } else {
+                console.log(err);
+                return [];
+            }
+        });
+    }, []);
+    useEffect(() => {
+        docClient.scan({ TableName: "FFXIV_Jobs"}, function(err, data) {
+            if (!err) {
+                setJobs(data.Items.sort(GetSortOrder("name")));
+            } else {
+                console.log(err);
+                return [];
+            }
+        });
+    }, []);
 
     function selectPatchItems(e) {
         const patchNumber = e.target.value;
@@ -48,33 +104,35 @@ function App() {
     }
 
     return (
-        <div className="App">
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="card m-5">
-                        <div className="card-header">Character Info</div>
-                        <div className="card-body">
-                            <CharacterInfo selectedJob={selectedJob} setSelectedJob={setSelectedJob} selectPatchItems={selectPatchItems} hasSoulCrystal={hasSoulCrystal} setHasSoulCrystal={setHasSoulCrystal} />
+        <div className="App bg-secondary">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="card text-bg-dark m-5">
+                            <h5 className="card-header text-bg-primary">Character Info</h5>
+                            <div className="card-body">
+                                <CharacterInfo jobs={jobs} selectedJob={selectedJob} setSelectedJob={setSelectedJob} selectPatchItems={selectPatchItems} hasSoulCrystal={hasSoulCrystal} setHasSoulCrystal={setHasSoulCrystal} />
+                            </div>
+                        </div>
+                        <div className="card text-bg-dark m-5">
+                            <h5 className="card-header text-bg-primary">Gear</h5>
+                            <div className="card-body">
+                                <GearSelect gearList={itemData} materia={materia} equippedGear={selectedGear} setEquippedGear={setSelectedGear} selectedJob={selectedJob} />
+                            </div>
                         </div>
                     </div>
-                    <div className="card m-5">
-                        <div className="card-header">Gear</div>
-                        <div className="card-body">
-                            <GearSelect gearList={itemData} materia={materia} equippedGear={selectedGear} setEquippedGear={setSelectedGear} selectedJob={selectedJob} />
+                    <div className="col-md-6">
+                        <div className="card text-bg-dark m-5">
+                            <h5 className="card-header text-bg-primary">Automations</h5>
+                            <div className="card-body">
+                                <CrafterAutomations automationConfig={automationConfig} setAutomationConfig={setAutomationConfig} />
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="card m-5">
-                        <div className="card-header">Automations</div>
-                        <div className="card-body">
-                            <CrafterAutomations />
-                        </div>
-                    </div>
-                    <div className="card m-5">
-                        <div className="card-header">Summary</div>
-                        <div className="card-body">
-                            <CrafterSummary equippedGear={selectedGear} selectedJob={selectedJob} hasSoulCrystal={hasSoulCrystal} />
+                        <div className="card text-bg-dark m-5">
+                            <h5 className="card-header text-bg-primary">Summary</h5>
+                            <div className="card-body">
+                                <CrafterSummary equippedGear={selectedGear} selectedJob={selectedJob} hasSoulCrystal={hasSoulCrystal} />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -120,6 +178,9 @@ function GearSelect({ gearList, materia, equippedGear, setEquippedGear, selected
 }
 
 function GearSlot({ slot, gearList, materia, equippedItem, selectedJob, selectGearForSlot, setEquippedGearMateria }) {
+    if (!gearList || !gearList.map || gearList.length === 0) {
+        return "";
+    }
     let slotOptions = gearList.map((item, index) => {
         if ((item.slot === slot || (item.slot === "Ring" && (slot === "LeftRing" || slot === "RightRing"))) && item.jobs.includes(selectedJob.code)) {
             return <option key={item.name} value={item.name}>{item.name}</option>
@@ -175,7 +236,7 @@ function GearSlot({ slot, gearList, materia, equippedItem, selectedJob, selectGe
             <label className="col-md-2 col-form-label">{slot}</label>
             <div className="col-md-7">
                 <div className="container-fluid">
-                    <div className="row">
+                    <div className="row pt-2 pb-2">
                         <label className="col-md-2 col-form-label text-end">Item</label>
                         <div className="col-md-10">
                             <select value={equippedItem.name} className="w-100 form-control" onChange={setSelectedItem}>
@@ -213,7 +274,7 @@ function MateriaSlot({ slotIndex, materia, selectedMateria, setMateriaOnItem, re
         return <option key={item.name} value={item.name}>{item.name} ({item.stat} + {item.value})</option>
     });
     return (
-        <div className="row">
+        <div className="row pb-2">
             <label className="col-sm-2 col-form-label text-end">Materia {slotIndex}</label>
             <div className="col-sm-6">
                 <select className="form-control" onChange={(e) => setMateriaOnItem(e, slotIndex - 1)} value={selectedMateria ? selectedMateria.name : ""}>
@@ -228,26 +289,82 @@ function MateriaSlot({ slotIndex, materia, selectedMateria, setMateriaOnItem, re
     )
 }
 
-function CrafterAutomations() {
+function CrafterAutomations( { automationConfig, setAutomationConfig } ) {
+    function setAutomationValue(value, stat, property) {
+        let newAutomationConfig = JSON.parse(JSON.stringify(automationConfig));
+        newAutomationConfig[stat][property] = value;
+        setAutomationConfig(newAutomationConfig);
+    }
+
+    function setFirstOvermeldRank(value) {
+        let newAutomationConfig = JSON.parse(JSON.stringify(automationConfig));
+        newAutomationConfig.firstOvermeldRank = value;
+        setAutomationConfig(newAutomationConfig);
+    }
+
+    function setOvermeldRank(value) {
+        let newAutomationConfig = JSON.parse(JSON.stringify(automationConfig));
+        newAutomationConfig.overmeldRank = value;
+        setAutomationConfig(newAutomationConfig);
+    }
+
     return (
         <div>
-            <div className="row">
-                <label htmlFor="targetControl" className="col-sm-1 col-form-label">Control</label>
+            <AutomationStat stat="control" automationConfig={automationConfig.control} setAutomationValue={setAutomationValue} />
+            <AutomationStat stat="craftsmanship" automationConfig={automationConfig.craftsmanship} setAutomationValue={setAutomationValue} />
+            <AutomationStat stat="cp" automationConfig={automationConfig.cp} setAutomationValue={setAutomationValue} />
+            <div className="row pt-2">
+                <label htmlFor="firstOvermeldMateriaRank" className="text-start col-sm-3 col-form-label">First Overmeld Rank</label>
                 <div className="col-sm-3">
-                    <input type="text" className="form-control" id="targetControl" placeholder="9999" />
-                </div>
-                <label htmlFor="targetCraftsmanship" className="col-sm-2 col-form-label">Craftsmanship</label>
-                <div className="col-sm-2">
-                    <input type="text" className="form-control" id="targetControl" placeholder="9999" />
-                </div>
-                <label htmlFor="targetCraftsmanship" className="col-sm-1 col-form-label">CP</label>
-                <div className="col-sm-3">
-                    <input type="text" className="form-control" id="targetControl" placeholder="9999" />
+                    <select id="firstOvermeldMateriaRank" className="form-select" onChange={(e) => setFirstOvermeldRank(e.target.value)} value={automationConfig.firstOvermeldRank}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </select>
                 </div>
             </div>
-            <div className="row">
+            <div className="row pt-2">
+                <label htmlFor="overmeldMateriaRank" className="text-start col-sm-3 col-form-label">Overmeld Rank</label>
+                <div className="col-sm-3">
+                    <select id="overmeldMateriaRank" className="form-select" onChange={(e) => setOvermeldRank(e.target.value)} value={automationConfig.overmeldRank}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="7">7</option>
+                        <option value="9">9</option>
+                    </select>
+                </div>
+            </div>
+            <div className="row pt-2">
                 <button className="btn btn-sm btn-primary col-sm-4 m-1">Clear Materia</button>
                 <button className="btn btn-sm btn-primary col-sm-4 m-1">Maximize Materia</button>
+            </div>
+        </div>
+    )
+}
+
+function AutomationStat( { stat, automationConfig, setAutomationValue } ) {
+    return (
+        <div className="row pt-2">
+            <div className="col-sm-1">
+                <i className="bi bi-chevron-expand"></i>
+            </div>
+            <label htmlFor={"target_" + stat} className="text-start col-sm-2 col-form-label">{stat}</label>
+            <div className="col-sm-3">
+                <input id={"target_" + stat} type="number" className="form-control" value={automationConfig.target} onChange={(e) => setAutomationValue(e.target.value, stat, "target")} />
+            </div>
+            <label htmlFor={"minimize_" + stat} className="text-start col-sm-2">Minimize?</label>
+            <div className="col-sm-1">
+                <input id={"minimize_" + stat} className="form-check-input" type="checkbox" checked={automationConfig.minimize} onChange={(e) => setAutomationValue(e.target.checked, stat, "minimize")}></input>
             </div>
         </div>
     )
@@ -275,22 +392,22 @@ function CrafterSummary({ equippedGear, selectedJob, hasSoulCrystal }) {
     return (
         <div>
             <div className="row">
-                <label className="col text-start col-sm-2">Control</label>
+                <label className="col text-start">Control</label>
                 <div className="col text-start">{control}</div>
             </div>
             <div className="row">
-                <label className="col text-start col-sm-2">Craftsmanship</label>
+                <label className="col text-start">Craftsmanship</label>
                 <div className="col text-start">{craftsmanship}</div>
             </div>
             <div className="row">
-                <label className="col text-start col-sm-2">CP</label>
+                <label className="col text-start">CP</label>
                 <div className="col text-start">{cp}</div>
             </div>
         </div>
     );
 }
 
-function CharacterInfo({ selectedJob, setSelectedJob, selectPatchItems, hasSoulCrystal, setHasSoulCrystal }) {
+function CharacterInfo({ jobs, selectedJob, setSelectedJob, selectPatchItems, hasSoulCrystal, setHasSoulCrystal }) {
 
     const jobOptions = jobs.map((item, index) => {
         return <option value={item.code} key={item.code}>{item.name} ({item.code})</option>
@@ -304,26 +421,32 @@ function CharacterInfo({ selectedJob, setSelectedJob, selectPatchItems, hasSoulC
     return (
         <div>
             <div className="row">
-                <label className="col text-start col-sm-2">Job</label>
-                <select className="col text-start col-sm-4" value={selectedJob.code} onChange={selectJob}>
-                    <option>Select a job</option>
-                    {jobOptions}
-                </select>
+                <label htmlFor="characterJob" className="text-start col-sm-2">Job</label>
+                <div className="col-sm-4">
+                    <select id="characterJob" className="form-select" value={selectedJob.code} onChange={selectJob}>
+                        <option>Select a job</option>
+                        {jobOptions}
+                    </select>
+                </div>
             </div>
             {selectedJob.name && (
                 <div className="row">
-                    <label className="col text-start col-sm-2">Patch</label>
-                    <select className="col text-start col-sm-4" onChange={selectPatchItems}>
-                        <option>Select a patch</option>
-                        <option>6.0</option>
-                        <option>6.3</option>
-                    </select>
+                    <label htmlFor="patch" className="text-start col-sm-2">Patch</label>
+                    <div className="col-sm-4">
+                        <select id="patch" className="form-select" onChange={selectPatchItems}>
+                            <option>Select a patch</option>
+                            <option>6.0</option>
+                            <option>6.3</option>
+                        </select>
+                    </div>
                 </div>
             )}
             {selectedJob.category === "DoH" && (
                 <div className="row">
-                    <label className="col text-start col-sm-2">Has Soul Crystal</label>
-                    <input className="col col-sm-4 form-check-input" type="checkbox" value={hasSoulCrystal} onChange={(e) => setHasSoulCrystal(e.target.checked)}></input>
+                    <label htmlFor="soulCrystal" className="col text-start col-sm-2">Has Soul Crystal</label>
+                    <div className="col-sm-4">
+                        <input id="soulCrystal" className="form-check-input" type="checkbox" checked={hasSoulCrystal} onChange={(e) => setHasSoulCrystal(e.target.checked)}></input>
+                    </div>
                 </div>
             )}
         </div>
